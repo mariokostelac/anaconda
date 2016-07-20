64cb314af1300fafe73f3abc8cdbddbcb7674701
Mario Kostelac <mario.kostelac@gmail.com>
2016-07-20 13:21:21 +0100

    Revert "Handle stream response reading error"
    This reverts commit 97e6e1ab24e95bee0276a10c39e3f27058d8e11f.

--- streaming.go
+++ streaming.go
@@ -173,12 +173,7 @@ func (s *Stream) listen(response http.Response) {
 	}
 
 	for s.run {
-		line, err := nextLine()
-		if err != nil {
-			s.api.Log.Error("Error while reading streaming response")
-			return
-		}
-
+		line, _ := nextLine()
 		if len(line) == 0 {
 			s.api.Log.Debug("Empty bytes... Moving along")
 		} else {
